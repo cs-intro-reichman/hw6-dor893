fa1d592aba1a6ea1a266e8b490ea096a9558d5ac
dor893 <dor.benartzi@post.runi.ac.il>
2024-12-17 11:28:06 +0200

    Editor1 - solved

--- Editor1.java
+++ Editor1.java
@@ -1,14 +1,14 @@
 import java.awt.Color;
 
-/**
- * Demonstrates three basic image processing operations that are featured by Runigram.java.
- * The program recieves two command-line arguments: a string representing the name of the PPM file
- * of a source image, and one of the strings "fh", "fv", or "gs". The program creates and displays
- * a new image which is either the horizontally flipped version of the source image ("fh"),
- * or the vertically flipped version of the source image ("fv"), or the grayscaled version of the
- * source image ("gs"). For example, to create a grayscale version of thor.ppm, use:
- * java Editor1 thor.ppm gs
- */
+
+ // Demonstrates three basic image processing operations that are featured by Runigram.java.
+ // The program recieves two command-line arguments: a string representing the name of the PPM file
+ // of a source image, and one of the strings "fh", "fv", or "gs". The program creates and displays
+ // a new image which is either the horizontally flipped version of the source image ("fh"),
+ // or the vertically flipped version of the source image ("fv"), or the grayscaled version of the
+ /// source image ("gs"). For example, to create a grayscale version of thor.ppm, use:
+ // java Editor1 thor.ppm gs
+ 
 public class Editor1 {
 
 	public static void main (String[] args){
